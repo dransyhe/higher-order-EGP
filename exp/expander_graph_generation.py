09e55fb1b13a81230db6699001f58e73e2231fd3
Thomas-Christie <thomashamish@hotmail.com>
2023-09-21 13:53:05 +0100

    Updated expander graph generation
    Updated expander graph generation to ensure that perfect matchings used
    to generate k perfect matchings are disjoint, and also fixed issue in
    Ramanujan graph generation.
    
    Also updated requirements.txt

--- exp/expander_graph_generation.py
+++ exp/expander_graph_generation.py
@@ -1,10 +1,9 @@
 import networkx as nx
 import math
 import numpy as np
-import random
 import torch
 from torch_geometric.data import Data
-from torch_geometric.utils import coalesce, convert, to_dense_adj, k_hop_subgraph
+from torch_geometric.utils import coalesce, convert, to_dense_adj
 
 
 def add_expander_edges_via_perfect_matchings(hypergraph_order: int,
@@ -34,19 +33,35 @@ def add_expander_edges_via_perfect_matchings(hypergraph_order: int,
     expander_graph_x = torch.concat((data.x, expander_graph_edge_nodes))
     new_num_nodes = expander_graph_x.shape[0]
 
-    source_nodes = torch.tensor([])
-    destination_nodes = torch.tensor([])
+    all_destination_nodes = torch.tensor([])
     for i in range(hypergraph_order):
-        left_nodes = torch.tensor([j for j in range(num_nodes)])
-        right_nodes = torch.tensor([num_nodes + j for j in range(num_nodes)])
-        left_perm = torch.randperm(left_nodes.shape[0])
-        right_perm = torch.randperm(right_nodes.shape[0])
-        source_nodes = torch.concat((source_nodes, left_nodes[left_perm]))
-        destination_nodes = torch.concat((destination_nodes, right_nodes[right_perm]))
+        valid_shuffle = False
+        while not valid_shuffle:
+            destination_nodes = torch.tensor([num_nodes + j for j in range(num_nodes)])
+            rand_perm = torch.randperm(destination_nodes.shape[0])
+            destination_nodes = destination_nodes[rand_perm]
+            if i == 0 or num_nodes < hypergraph_order:
+                # If there are fewer nodes than the order of the hypergraph, we can't avoid duplicate edges in the
+                # constructed bipartite graph
+                valid_shuffle = True
+            else:
+                # Checks to ensure that all hyperedges consist of 'hypergraph_order' unique nodes so bipartite graph is regular
+                valid_shuffle = True
+                for j in range(hypergraph_order - 1, 0, -1):
+                    nodes_in_edge = torch.cat((all_destination_nodes[-j:], destination_nodes[:hypergraph_order - j]))
+                    valid_shuffle = valid_shuffle and (torch.unique(nodes_in_edge).shape[0] == nodes_in_edge.shape[0])
+        all_destination_nodes = torch.cat((all_destination_nodes, destination_nodes))
 
-    expander_edge_index = torch.cat([source_nodes[None, ...], destination_nodes[None, ...]], dim=0)
+    all_source_nodes = [i for i in range(num_nodes)]
+    all_source_nodes = torch.tensor(np.repeat(all_source_nodes, hypergraph_order).tolist())
+
+    expander_edge_index = torch.cat((all_source_nodes[None, ...], all_destination_nodes[None, ...]), dim=0)
     expander_edge_index = coalesce(expander_edge_index)  # Remove duplicate edges
     expander_edge_index = expander_edge_index.to(torch.int64)
+    if num_nodes >= hypergraph_order:
+            # If there are more (or equal) nodes in the original graph than the hypergraph order, then there should
+            # be 'hypergraph_order' * 'num_nodes' unique edges in the graph
+            assert expander_edge_index.shape[1] == hypergraph_order * num_nodes
 
     ones = torch.ones(num_nodes)
     zeros = torch.zeros(num_nodes)
@@ -65,7 +80,6 @@ def add_expander_edges_via_perfect_matchings(hypergraph_order: int,
 
 
 def add_expander_edges_via_ramanujan_bipartite_graph(hypergraph_order: int,
-                                                     random_seed: int,
                                                      dataset: str,
                                                      data: Data):
     """
@@ -76,11 +90,10 @@ def add_expander_edges_via_ramanujan_bipartite_graph(hypergraph_order: int,
      'edge nodes' to the original graph nodes in 'data.x', and add an 'expander_node_mask' attribute, where
      expander_node_mask[i] == 1 if data.x[i] is a node belonging to the original graph, and is 0 if data.x[i] is an
      'edge node' belonging to the expander graph. We test to ensure that the bipartite expander graph produced is fully
-     connected and satisfies the Ramanujan property (https://en.wikipedia.org/wiki/Ramanujan_graph) making it a good
+     connected and satisfies the Ramanujan property (https://en.wikipedia.org/wiki/Ramanujan_graph), making it a good
      candidate for an expander graph.
 
      :param hypergraph_order: number of perfect matchings to generate. This is the order of the resulting 'hypergraph'.
-     :param random_seed: random seed for generating the expander graphs.
      :param data: graph to be augmented
      :param dataset: dataset which is being augmented
      :return: updated graph with additional attributes for expander graph
@@ -96,15 +109,14 @@ def add_expander_edges_via_ramanujan_bipartite_graph(hypergraph_order: int,
 
     connected = False
     ramanujan = False
-    random_seed_offset = 0
-    while (not connected) and (not ramanujan):
-        random.seed(random_seed + random_seed_offset)
-        all_destination_nodes = []
+    while not (connected and ramanujan):
+        all_destination_nodes = torch.tensor([])
         for i in range(hypergraph_order):
             valid_shuffle = False
             while not valid_shuffle:
-                destination_nodes = [num_nodes + j for j in range(num_nodes)]
-                random.shuffle(destination_nodes)
+                destination_nodes = torch.tensor([num_nodes + j for j in range(num_nodes)])
+                rand_perm = torch.randperm(destination_nodes.shape[0])
+                destination_nodes = destination_nodes[rand_perm]
                 if i == 0 or num_nodes < hypergraph_order:
                     # If there are fewer nodes than the order of the hypergraph, we can't avoid duplicate edges in the
                     # constructed bipartite graph
@@ -113,15 +125,14 @@ def add_expander_edges_via_ramanujan_bipartite_graph(hypergraph_order: int,
                     # Checks to ensure that all hyperedges consist of 'hypergraph_order' unique nodes so bipartite graph is regular
                     valid_shuffle = True
                     for j in range(hypergraph_order - 1, 0, -1):
-                        nodes_in_edge = all_destination_nodes[-j:] + destination_nodes[:hypergraph_order - j]
-                        valid_shuffle = valid_shuffle and (len(nodes_in_edge) == len(set(nodes_in_edge)))
-            all_destination_nodes.extend(destination_nodes)
+                        nodes_in_edge = torch.cat((all_destination_nodes[-j:], destination_nodes[:hypergraph_order - j]))
+                        valid_shuffle = valid_shuffle and (torch.unique(nodes_in_edge).shape[0] == nodes_in_edge.shape[0])
+            all_destination_nodes = torch.cat((all_destination_nodes, destination_nodes))
 
         all_source_nodes = [i for i in range(num_nodes)]
         all_source_nodes = torch.tensor(np.repeat(all_source_nodes, hypergraph_order).tolist())
-        all_destination_nodes = torch.tensor(all_destination_nodes)
 
-        expander_edge_index = torch.cat([all_source_nodes[None, ...], all_destination_nodes[None, ...]], dim=0)
+        expander_edge_index = torch.cat((all_source_nodes[None, ...], all_destination_nodes[None, ...]), dim=0)
         expander_edge_index = coalesce(expander_edge_index)
         expander_edge_index = expander_edge_index.to(torch.int64)
         if num_nodes >= hypergraph_order:
@@ -137,11 +148,10 @@ def add_expander_edges_via_ramanujan_bipartite_graph(hypergraph_order: int,
             adj_eigenvalues = np.sort(np.linalg.eigvals(adj_matrix.toarray()))
             second_largest_eigenvalue = max(abs(adj_eigenvalues[1]), adj_eigenvalues[-2])
             ramanujan = second_largest_eigenvalue <= 2 * math.sqrt(hypergraph_order - 1)
-            random_seed_offset += 1
         else:
-            connected = True
-            ramanujan = True
+            raise ValueError("Ramanujan property cannot currently be checked for code2 and ppa graphs due to computational costs.")
 
+    assert ramanujan and connected
     ones = torch.ones(num_nodes)
     zeros = torch.zeros(num_nodes)
     expander_node_mask = torch.concat((ones, zeros))
