214794d9ac2ca23a20866412202fcc0a69fa2cbc
Thomas-Christie <thomashamish@hotmail.com>
2023-09-26 00:55:06 +0100

    Ensure k-regular bipartite graphs generated via perfect matchings are uniform random

--- exp/expander_graph_generation.py
+++ exp/expander_graph_generation.py
@@ -12,16 +12,14 @@ def add_expander_edges_via_perfect_matchings(hypergraph_order: int,
     """
     Augments graph in 'data' with a new bipartite graph representation of a hypergraph for use as an expander graph.
     For each node in the original graph, we add a node in the bipartite graph. We then generate 'hypergraph_order'
-    perfect matchings of the resulting bipartite graph, and store these edges in the 'expander_edge_index' attribute
+    disjoint perfect matchings of the resulting bipartite graph, and store these edges in the 'expander_edge_index' attribute
     of the 'data'. We add the expander graph 'edge nodes' to the original graph nodes in 'data.x', and add an
     'expander_node_mask' attribute, where expander_node_mask[i] == 1 if data.x[i] is a node belonging to the original
     graph, and is 0 if data.x[i] is an 'edge node' belonging to the expander graph.
 
-    :param hypergraph_order: number of perfect matchings to generate. This is approximately the order of the resulting
-                             'hypergraph', though some 'edges' may be of a lower order as we don't enforce that the
-                             matchings are disjoint.
-    :param data: graph to be augmented
+    :param hypergraph_order: number of perfect matchings to generate. This is the order of the resulting 'hypergraph'.
     :param dataset: dataset which is being augmented
+    :param data: graph to be augmented
     :return: updated graph with additional attributes for expander graph
     """
     if dataset == "ppa":
@@ -33,27 +31,31 @@ def add_expander_edges_via_perfect_matchings(hypergraph_order: int,
     expander_graph_x = torch.concat((data.x, expander_graph_edge_nodes))
     new_num_nodes = expander_graph_x.shape[0]
 
-    all_destination_nodes = torch.tensor([], dtype=torch.int64)
-    for i in range(hypergraph_order):
-        valid_shuffle = False
-        while not valid_shuffle:
-            destination_nodes = torch.tensor([num_nodes + j for j in range(num_nodes)])
+    destination_node_permutations = []
+    # Generate `hypergraph_order` disjoint perfect matchings
+    for matching_num in range(hypergraph_order):
+        disjoint_matching = False
+        destination_nodes = torch.tensor([num_nodes + j for j in range(num_nodes)])
+        while not disjoint_matching:
             rand_perm = torch.randperm(destination_nodes.shape[0])
             destination_nodes = destination_nodes[rand_perm]
-            if i == 0 or num_nodes < hypergraph_order:
+            if num_nodes < hypergraph_order:
                 # If there are fewer nodes than the order of the hypergraph, we can't avoid duplicate edges in the
                 # constructed bipartite graph
-                valid_shuffle = True
+                disjoint_matching = True
             else:
-                # Checks to ensure that all hyperedges consist of 'hypergraph_order' unique nodes so bipartite graph is regular
-                valid_shuffle = True
-                for j in range(hypergraph_order - 1, 0, -1):
-                    nodes_in_edge = torch.cat((all_destination_nodes[-j:], destination_nodes[:hypergraph_order - j]))
-                    valid_shuffle = valid_shuffle and (torch.unique(nodes_in_edge).shape[0] == nodes_in_edge.shape[0])
-        all_destination_nodes = torch.cat((all_destination_nodes, destination_nodes))
+                # Checks to ensure that the matching is disjoint to all previous
+                # matchings so that the generated hypergraph is regular
+                disjoint_matching = True
+                for i in range(matching_num):
+                    disjoint_matching = disjoint_matching and (destination_nodes != destination_node_permutations[i]).all()
+                    if not disjoint_matching:
+                        break
+        destination_node_permutations.append(destination_nodes)
 
-    all_source_nodes = [i for i in range(num_nodes)]
-    all_source_nodes = torch.tensor(np.repeat(all_source_nodes, hypergraph_order).tolist())
+    all_destination_nodes = torch.hstack(destination_node_permutations)
+    all_source_nodes = torch.randperm(num_nodes)
+    all_source_nodes = all_source_nodes.repeat(hypergraph_order)
 
     expander_edge_index = torch.cat((all_source_nodes[None, ...], all_destination_nodes[None, ...]), dim=0)
     expander_edge_index = coalesce(expander_edge_index)  # Remove duplicate edges
@@ -93,8 +95,8 @@ def add_expander_edges_via_ramanujan_bipartite_graph(hypergraph_order: int,
      candidate for an expander graph.
 
      :param hypergraph_order: number of perfect matchings to generate. This is the order of the resulting 'hypergraph'.
-     :param data: graph to be augmented
      :param dataset: dataset which is being augmented
+     :param data: graph to be augmented
      :return: updated graph with additional attributes for expander graph
      """
     if dataset == "ppa":
@@ -109,27 +111,31 @@ def add_expander_edges_via_ramanujan_bipartite_graph(hypergraph_order: int,
     connected = False
     ramanujan = False
     while not (connected and ramanujan):
-        all_destination_nodes = torch.tensor([], dtype=torch.int64)
-        for i in range(hypergraph_order):
-            valid_shuffle = False
-            while not valid_shuffle:
-                destination_nodes = torch.tensor([num_nodes + j for j in range(num_nodes)])
+        destination_node_permutations = []
+        # Generate `hypergraph_order` disjoint perfect matchings
+        for matching_num in range(hypergraph_order):
+            disjoint_matching = False
+            destination_nodes = torch.tensor([num_nodes + j for j in range(num_nodes)])
+            while not disjoint_matching:
                 rand_perm = torch.randperm(destination_nodes.shape[0])
                 destination_nodes = destination_nodes[rand_perm]
-                if i == 0 or num_nodes < hypergraph_order:
+                if num_nodes < hypergraph_order:
                     # If there are fewer nodes than the order of the hypergraph, we can't avoid duplicate edges in the
                     # constructed bipartite graph
-                    valid_shuffle = True
+                    disjoint_matching = True
                 else:
-                    # Checks to ensure that all hyperedges consist of 'hypergraph_order' unique nodes so bipartite graph is regular
-                    valid_shuffle = True
-                    for j in range(hypergraph_order - 1, 0, -1):
-                        nodes_in_edge = torch.cat((all_destination_nodes[-j:], destination_nodes[:hypergraph_order - j]))
-                        valid_shuffle = valid_shuffle and (torch.unique(nodes_in_edge).shape[0] == nodes_in_edge.shape[0])
-            all_destination_nodes = torch.cat((all_destination_nodes, destination_nodes))
+                    # Checks to ensure that the matching is disjoint to all previous
+                    # matchings so that the generated hypergraph is regular
+                    disjoint_matching = True
+                    for i in range(matching_num):
+                        disjoint_matching = disjoint_matching and (destination_nodes != destination_node_permutations[i]).all()
+                        if not disjoint_matching:
+                            break
+            destination_node_permutations.append(destination_nodes)
 
-        all_source_nodes = [i for i in range(num_nodes)]
-        all_source_nodes = torch.tensor(np.repeat(all_source_nodes, hypergraph_order).tolist())
+        all_destination_nodes = torch.hstack(destination_node_permutations)
+        all_source_nodes = torch.randperm(num_nodes)
+        all_source_nodes = all_source_nodes.repeat(hypergraph_order)
 
         expander_edge_index = torch.cat((all_source_nodes[None, ...], all_destination_nodes[None, ...]), dim=0)
         expander_edge_index = coalesce(expander_edge_index)
